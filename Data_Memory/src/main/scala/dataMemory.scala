e57d3ec26be622527245c6afc4e9b4fb4af0b89b
TharukaN17 <tharukanilupul@gmail.com>
2021-10-05 12:58:53 +0530

    Initiate a memory

---
+++ Data_Memory/src/main/scala/dataMemory.scala
@@ -0,0 +1,25 @@
+import chisel3._
+import chisel3.util._
+import chisel3.Driver
+
+class Memory() extends Module{
+    val io = IO(new Bundle{
+        val rdAddr = Input(UInt(10.W))
+        val rdData = Output(UInt(8.W))
+        val wrEna = Input(Bool())
+        val wrData = Input(UInt(8.W))
+        val wrAddr = Input(UInt(10.W))
+    })
+
+    val mem = SyncReadMem(1024, UInt(8.W))
+
+    io.rdData := mem.read(io.rdAddr)
+
+    when(io.wrEna){
+        mem.write(io.wrAddr, io.wrData)
+    }
+}
+
+object Memory extends App{
+    (new chisel3.stage.ChiselStage).emitVerilog(new Memory())
+}
