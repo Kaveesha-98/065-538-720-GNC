ea9e1fe013bbf1850456b501832d97267ed61c1d
TharukaN17 <tharukanilupul@gmail.com>
2021-10-14 19:16:44 +0530

    Initiated ALU folder

---
+++ ALU/src/main/scala/ALU.scala
@@ -0,0 +1,12 @@
+import chisel3._
+import chisel3.util._
+import chisel3.Driver
+
+class ALU extends Module {
+  
+
+}
+
+object Memory extends App{
+    (new chisel3.stage.ChiselStage).emitVerilog(new Memory())
+}
