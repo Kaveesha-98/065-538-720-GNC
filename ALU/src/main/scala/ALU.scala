1661f95f8ddb673987e11665835b98482409c3f0
TharukaN17 <tharukanilupul@gmail.com>
2021-10-14 19:38:09 +0530

    Copied the layout from kaveesha and set global constants

--- ALU/src/main/scala/ALU.scala
+++ ALU/src/main/scala/ALU.scala
@@ -2,11 +2,37 @@ import chisel3._
 import chisel3.util._
 import chisel3.Driver
 
+// Global constats
+object constants {
+  //ALU Operations
+  val add = 0.U(3.W)
+  val sub = 1.U(3.W)
+  val sll = 2.U(3.W)
+  val sra = 3.U(3.W)
+  val srl = 4.U(3.W)
+  val xor = 5.U(3.W)
+  val or  = 6.U(3.W)
+  val and = 7.U(3.W)
+}
+
 class ALU extends Module {
-  
+    val io = IO(new Bundle{
+    	//Control unit connections
+        val ALU_OP = Input(UInt(3.W)) //ALUOperations{add, sub, sll, sra, srl, xor, or, and}
+        val EQUAL = Output(UInt(1.W)) //When two registers are equal
+        val LESS_THAN = Output(UInt(1.W)) // When input1 is less than input 2 
+        //Datapath connections
+        val ALUinput1 = Input(SInt(32.W))
+        val ALUinput2 = Input(SInt(32.W))
+        val ALUoutput = Output(SInt(32.W))
+    })
+    //Giving default values for outputs so no error happens when compiling
+    io.ALUoutput := 0.S
+    io.EQUAL := 1.U
+    io.LESS_THAN := 0.U
 
 }
 
-object Memory extends App{
-    (new chisel3.stage.ChiselStage).emitVerilog(new Memory())
+object ALU extends App{
+    (new chisel3.stage.ChiselStage).emitVerilog(new ALU())
 }
