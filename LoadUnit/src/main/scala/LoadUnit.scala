a2393e8feccb0a555a99ebd2c6e0f797dedaeeed
Charith Janaka <64123653+charithjanaka@users.noreply.github.com>
2021-10-17 15:52:38 +0530

    Update LoadUnit.scala

--- LoadUnit/src/main/scala/LoadUnit.scala
+++ LoadUnit/src/main/scala/LoadUnit.scala
@@ -4,14 +4,14 @@ import chisel3.util._
 class Load_Unit extends Module{
 	val io = IO(new Bundle{
 		
-    val load_mem_address_in = Input(UInt(32.W))				// Corresponding memory address to Load Data - Input
-    val load_data = Input(SInt(32.W))						// Loaded Data into Load Unit		
+    	val load_mem_address_in = Input(UInt(32.W))				// Corresponding memory address to Load Data - Input
+    	val load_data = Input(SInt(32.W))						// Loaded Data into Load Unit		
     
 	val LOAD_TO_REG = Input(UInt(1.W))						// Control signal to load data from Load Unit to a Register
 	val LOAD_DATA_SIZE = Input(UInt(2.W))					// Control signal containing the size of the data to be loaded
 	val ADDRESS_EN = Input(UInt(1.W))						// Control signal to enable mem read address to Load Unit
     
-    val mem_read = Output(UInt(1.W))						// Control signal to load data
+    	val mem_read = Output(UInt(1.W))						// Control signal to load data
 	val LOAD_UNIT_READY = Output(UInt(1.W))					// Status of Load Unit
 
 	val load_mem_address_out = Output(UInt(32.W))			// Corresponding memory address to Load Data - Output
@@ -50,11 +50,11 @@ class Load_Unit extends Module{
 		io.mem_read := 1.U
 		count_begin := 1.U
 		count_begin := io.mem_read
-        when(load_data_size_buffer === 3.U){
+        	when(load_data_size_buffer === 3.U){
 			stateReg := ready                           
 			reading := absent_r
-        }.otherwise{
-            load_data_address_buffer := load_data_address_buffer + 1.U
+        	}.otherwise{
+            		load_data_address_buffer := load_data_address_buffer + 1.U
 			load_data_size_buffer := load_data_size_buffer + 1.U
 		}
 		
@@ -63,16 +63,16 @@ class Load_Unit extends Module{
 		io.mem_read := 0.U
 	}
 	
-    when(loading === present_l){
-        load_data_buffer := io.load_data
-        when(load_data_size_buffer === 3.U){
+    	when(loading === present_l){
+        	load_data_buffer := io.load_data
+        	when(load_data_size_buffer === 3.U){
 			stateReg := ready                          
 			loading := absent_l
-        }.otherwise{
-            load_data_buffer := load_data_buffer << 8
+        	}.otherwise{
+            		load_data_buffer := load_data_buffer << 8
 			load_data_size_buffer := load_data_size_buffer + 1.U
 		}
-    }.otherwise{
+    	}.otherwise{
 		io.load_data := 0.S
 	}
 
