0911175a13ca74dedfa4dc026e80c23b52d53a09
TharukaN17 <tharukanilupul@gmail.com>
2021-10-17 10:51:52 +0530

    Add signed less than output

--- ALU/src/test/scala/ALUTest.scala
+++ ALU/src/test/scala/ALUTest.scala
@@ -6,18 +6,20 @@ class Tester(dut: ALU) extends PeekPokeTester(dut) {
 
     println("clk cycle: " + 0.toString())
     println("Output: " + peek(dut.io.ALUoutput).toString)
-    println("Zero flag: " + peek(dut.io.EQUAL).toString)
-    println("Negative flag: " + peek(dut.io.LESS_THAN).toString)
+    println("Equal: " + peek(dut.io.EQUAL).toString)
+    println("Less than signed: " + peek(dut.io.SIGNED_LESS_THAN).toString)
+    println("Less than unsigned: " + peek(dut.io.LESS_THAN).toString)
     println("")
 
     step(1)
-    poke(dut.io.ALU_OP,2.U)
-    poke(dut.io.ALUinput1,10.S)
-    poke(dut.io.ALUinput2,2.S)
+    poke(dut.io.ALU_OP,0.U)
+    poke(dut.io.ALUinput1,-5.S)
+    poke(dut.io.ALUinput2,10.S)
     println("clk cycle: " + 1.toString())
     println("Output: " + peek(dut.io.ALUoutput).toString)
-    println("Zero flag: " + peek(dut.io.EQUAL).toString)
-    println("Negative flag: " + peek(dut.io.LESS_THAN).toString)
+    println("Equal: " + peek(dut.io.EQUAL).toString)
+    println("Less than signed: " + peek(dut.io.SIGNED_LESS_THAN).toString)
+    println("Less than unsigned: " + peek(dut.io.LESS_THAN).toString)
     println("")
     
 }
