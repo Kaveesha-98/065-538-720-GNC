35454b4c200d412a7d6c0035e9cf0c3c3f55f4ab
TharukaN17 <tharukanilupul@gmail.com>
2021-11-08 17:37:21 +0530

    Updated the test for more cycles

--- Prefetch_Unit/src/test/scala/preFetchTest.scala
+++ Prefetch_Unit/src/test/scala/preFetchTest.scala
@@ -3,43 +3,48 @@ import chisel3.iotesters.PeekPokeTester
 import org.scalatest._
 
 class Tester(dut: Prefetch ) extends PeekPokeTester(dut) {
-    poke(dut.io.pc,2.U)
-    poke(dut.io.BRANCH,0.U)
-    poke(dut.io.instr_in,2.U)
+
+    var i = 0
+    for (i <- 0 to 10){
+      poke(dut.io.pc,5.U)
+      poke(dut.io.BRANCH,0.U)
+      poke(dut.io.instr_in,i.U)
+      step(1)
+      println("clk cycle: " + (i+1).toString)
+      println("Instruction: " + peek(dut.io.instr_out).toString)
+      println("Address: " + peek(dut.io.addr_out).toString)
+      println("Valid: " + peek(dut.io.VALID).toString)
+      println("")
+    }
+
+    poke(dut.io.pc,25.U)
+    poke(dut.io.BRANCH,1.U)
+    poke(dut.io.instr_in,11.U)
     step(1)
-    println("clk cycle: " + 1.toString)
+    println("clk cycle: " + 11.toString)
     println("Instruction: " + peek(dut.io.instr_out).toString)
     println("Address: " + peek(dut.io.addr_out).toString)
     println("Valid: " + peek(dut.io.VALID).toString)
     println("")
 
-    step(1)
-    println("clk cycle: " + 1.toString)
-    println("Instruction: " + peek(dut.io.instr_out).toString)
-    println("Address: " + peek(dut.io.addr_out).toString)
-    println("Valid: " + peek(dut.io.VALID).toString)
-    println("")
-
-    step(1)
-    println("clk cycle: " + 1.toString)
-    println("Instruction: " + peek(dut.io.instr_out).toString)
-    println("Address: " + peek(dut.io.addr_out).toString)
-    println("Valid: " + peek(dut.io.VALID).toString)
-    println("")
-
-    step(1)
-    println("clk cycle: " + 1.toString)
-    println("Instruction: " + peek(dut.io.instr_out).toString)
-    println("Address: " + peek(dut.io.addr_out).toString)
-    println("Valid: " + peek(dut.io.VALID).toString)
-    println("")
+    for (i <- 25 to 35){
+      poke(dut.io.pc,30.U)
+      poke(dut.io.BRANCH,0.U)
+      poke(dut.io.instr_in,i.U)
+      step(1)
+      println("clk cycle: " + (i-13).toString)
+      println("Instruction: " + peek(dut.io.instr_out).toString)
+      println("Address: " + peek(dut.io.addr_out).toString)
+      println("Valid: " + peek(dut.io.VALID).toString)
+      println("")
+    }
     
 }
 
 object Tester extends App {
-	chisel3.iotesters.Driver(() => new Prefetch()) { c => 
-	new Tester(c)
-	}
+  iotesters.Driver.execute(Array("--target-dir", "generated", "--generate-vcd-output", "on"), () => new Prefetch()) {
+    c => new Tester(c)
+  }
 }
 
 class SimpleSpec extends FlatSpec with Matchers {
