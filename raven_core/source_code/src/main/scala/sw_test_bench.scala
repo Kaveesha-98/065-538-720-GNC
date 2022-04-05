022b5586fc9829ec4643a023f0390211bb65d32d
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2022-04-05 13:52:58 +0530

    sw memory interface

--- raven_core/source_code/src/main/scala/sw_test_bench.scala
+++ raven_core/source_code/src/main/scala/sw_test_bench.scala
@@ -4,10 +4,14 @@ import chisel3.Driver
 
 class channel_a_buffer(z: Int, o: Int, a: Int, w: Int) extends Module {
     val io = IO(new Bundle{
-        val channel_a_in = Flipped(new channel_a(z, o, a, w))
+        val channel_a_in = Flipped(new channel_a(4, o, a, w))
         val channel_a_out = new channel_a(z, o, a, w)
         val update = Input(Bool())
+        val reset = Input(Bool())
     })
+
+    val empty :: full :: Nil = Enum(2)
+    val stateReg = RegInit(empty)
 
     val opcode 	= Reg(UInt(3.W))
 	val param 	= Reg(UInt(3.W))
@@ -17,7 +21,8 @@ class channel_a_buffer(z: Int, o: Int, a: Int, w: Int) extends Module {
 	val mask 	= Reg(UInt(w.W))
 	val data 	= Reg(UInt((8*w).W))
 
-    io.channel_a_in.ready := true.B
+    io.channel_a_in.ready := stateReg === empty
+    io.channel_a_out.valid := stateReg === full
 
     when(io.update){
         opcode := io.channel_a_in.opcode
@@ -27,6 +32,9 @@ class channel_a_buffer(z: Int, o: Int, a: Int, w: Int) extends Module {
         address := io.channel_a_in.address
         mask := io.channel_a_in.mask
         data := io.channel_a_in.data
+        stateReg := full
+    }.elsewhen(io.reset){
+        stateReg := empty
     }
 
     io.channel_a_out.opcode := opcode
@@ -37,7 +45,6 @@ class channel_a_buffer(z: Int, o: Int, a: Int, w: Int) extends Module {
     io.channel_a_out.mask := mask
     io.channel_a_out.data := data
 
-    io.channel_a_out.valid := true.B
 }
 
 class sw_testbench_memory_interface(addrWidth: Int) extends Bundle {
@@ -49,37 +56,64 @@ class sw_testbench_memory_interface(addrWidth: Int) extends Bundle {
     val write_enable = Output(Bool())
 }
 
-class sw_testbench_memory_controller(z: Int, a_o: Int, a_a: Int, w: Int, d_o: Int, d_i: Int) extends Module {
+class sw_testbench_memory_controller(sourceBits: Int) extends Module {
     val io = IO(new Bundle{
-        val channel_a_in = Flipped(new channel_a(z, a_o, a_a, w))
-        val channel_d_out = new channel_d(z, d_o, d_i, w)
-        val memory_channel = new sw_testbench_memory_interface(a_a)
+        //contacts only with two masters
+        val channel_a_data = Flipped(new channel_a(4, sourceBits, 32, 4))
+        val channel_a_instruction = Flipped(new channel_a(4, sourceBits, 32, 4))
+        val channel_d_out = new channel_d(4, sourceBits, 1, 4)
+        val memory_channel = new sw_testbench_memory_interface(32)
     })
 
-    val a_ready :: data_transfer :: d_valid :: Nil = Enum(3)
+    val a_ready :: data_get :: Nil = Enum(2)
     val stateReg = RegInit(a_ready)
-    val byte_size = Reg(UInt())
 
-    val channel_a_in_buffer = Module(new channel_a_buffer(z, a_o, a_a, w))
-    channel_a_in_buffer.io.channel_a_in <> io.channel_a_in
+    val buffer_channel_a_data = Module(new channel_a_buffer(4, 1, 32, 4))
+    val buffer_channel_a_instruction = Module(new channel_a_buffer(4, 1, 32, 4))
 
-    channel_a_in_buffer.io.update := false.B
+    buffer_channel_a_data.io.channel_a_in <> io.channel_a_data
+    buffer_channel_a_instruction.io.channel_a_in <> io.channel_a_instruction
 
-    val address_buffer = Reg(UInt(a_a.W))
-    val cntReg = Reg(UInt(scala.math.pow(2,z).toInt.W))
+    val op_source = Reg(UInt(sourceBits.W))
+    val addrLSBShift = Reg(UInt(8.W))
+
+    io.channel_a_data.ready := false.B
+    io.channel_a_instruction := false.B
+
+    val channel_inst :: channel_data :: Nil = Enum(2)
 
     switch(stateReg){
         is(a_ready){
-            //continously read and store input channel a
-            io.channel_a_in.ready := true.B
-            channel_a_in_buffer.io.update := true.B
-            when(io.channel_a_in.valid){
-                switch(io.channel_a_in.opcode){
+            io.channel_a_data.ready := true.B
+            io.channel_a_instruction.ready := true.B
+            buffer_channel_a_data.io.update := true.B
+            buffer_channel_a_instruction.updte := true.B
+            when(io.channel_a_instruction.valid){
+                op_source := channel_inst
+                switch(io.channel_a_instruction.opcode){
                     is(channel_a_opcodes.GET){
-
+                        stateReg := data_get
+                        addrLSBShift := "b11100100".U
+                    }
+                }
+            }.elsewhen(io.channel_a_data.valid){
+                op_source := channel_data
+                switch(io.channel_a_data.opcode){
+                    is(channel_a_opcodes.GET){
+                        stateReg := data_get
+                        addrLSBShift := "b11100100".U
                     }
                 }
             }
         }
+        is(data_get){
+            when(addrLSBShift(1, 0) === 3.U){
+                addrLSBShift := Cat(addrLSBShift(1, 0), addrLSBShift(7, 2))
+            }
+        }
     }
+
+    val read_base_address := Mux(op_source === channel_inst, io.channel_a_instruction.address, io.channel_a_data.address)
+
+    io.memory_channel.read_address := Cat(read_base_address(31, 2), addrLSBShift(1, 0))
 }
