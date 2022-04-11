a6bbaec539db5c1ab42d1f84e95859019469edbd
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2022-04-11 11:21:42 +0530

    memory interface

--- raven_core/source_code/src/main/scala/sw_test_bench.scala
+++ raven_core/source_code/src/main/scala/sw_test_bench.scala
@@ -65,7 +65,44 @@ class sw_testbench_memory_controller(sourceBits: Int) extends Module {
         val memory_channel = new sw_testbench_memory_interface(32)
     })
 
-    val a_ready :: data_get :: Nil = Enum(2)
+    def twoBitIncrement[T <: Data](bitCnt: T): T = {
+        val nextBitCnt0 = ~bitCnt(0)
+        val nextBitCnt1 = Mux(bitCnt(0), ~bitCnt(1), bitCnt(1))
+        val nextBitCnt  = Cat(nextBitCnt1, nextBitCnt0)
+        nextBitCnt
+    }
+
+    def nBitIncrement[T <: Data](bitCnt: T, with: Int): T = {
+        val partialProducts = new Array[chisel3.UInt]((width)
+
+        for(i <- 0 until width){
+            partialProducts(i) = Wire(UInt(1.W))
+        }
+
+        partialProducts(0) = true.B
+        partialProducts(1) = bitCnt(0).asBool
+
+        for(i <- 2 until width){
+            partialProducts(i) := bitCnt(i-1, 0).andR
+        }
+
+        val nextBitCntArray = new Array[chisel3.UInt](width)
+
+        for(i <- 0 until width){
+            nextBitCntArray(i) = Wire(UInt(1.W))
+
+            nextBitCntArray(i) := Mux(partialProducts(i), ~bitCnt(i), bitCnt)
+        }
+
+        val nextBitCnt = Cat(nextBitCntArray.reverse)
+        nextBitCnt
+    }
+
+    val memAddress = Reg(UInt(32.W))
+
+    val newMemAddress = nBitIncrement(memAddress, 32)
+
+    val a_ready :: data_get_start :: data_get_cont :: Nil = Enum(2)
     val stateReg = RegInit(a_ready)
 
     val buffer_channel_a_data = Module(new channel_a_buffer(4, 1, 32, 4))
@@ -74,41 +111,73 @@ class sw_testbench_memory_controller(sourceBits: Int) extends Module {
     buffer_channel_a_data.io.channel_a_in <> io.channel_a_data
     buffer_channel_a_instruction.io.channel_a_in <> io.channel_a_instruction
 
+    //opsource indicates the which channels request is being addressed
     val op_source = Reg(UInt(sourceBits.W))
-    val addrLSBShift = Reg(UInt(8.W))
+    val bitCnt = Reg(UInt(2.W))
 
-    io.channel_a_data.ready := false.B
-    io.channel_a_instruction := false.B
+    io.channel_a_data.ready     := false.B
+    io.channel_a_instruction    := false.B
+
+    buffer_channel_a_data.io.update     := false.B
+    buffer_channel_a_instruction.update := false.B
 
     val channel_inst :: channel_data :: Nil = Enum(2)
 
+    val nextBitCnt = twoBitIncrement(bitCnt)
+
+    val dataBuffer = Reg(Vec(4, UInt (8.W)))
+
+    val size = Reg(UInt(2.W))
+
+    io.memory_channel.read_address  := memAddress
+    io.memory_channel.write_address := memAddress
+
+    /*when both instruction and data channel issues a request
+    instruction channel is given priority*/
     switch(stateReg){
         is(a_ready){
+            //can take requests from instruction channel and data channel
             io.channel_a_data.ready := true.B
             io.channel_a_instruction.ready := true.B
+            //buffer requests from instruction and data channels on every channels
             buffer_channel_a_data.io.update := true.B
             buffer_channel_a_instruction.updte := true.B
             when(io.channel_a_instruction.valid){
-                op_source := channel_inst
+                //when the instruction channel issues a request
+                op_source   := channel_inst
+                memAddress  := io.channel_a_instruction.address
+                size        := 3.U
                 switch(io.channel_a_instruction.opcode){
                     is(channel_a_opcodes.GET){
-                        stateReg := data_get
-                        addrLSBShift := "b11100100".U
+                        stateReg := data_get_start
+                        bitCnt := 0.U
                     }
                 }
             }.elsewhen(io.channel_a_data.valid){
-                op_source := channel_data
+                op_source   := channel_data
+                memAddress  := io.channel_a_data.address
+                switch(io.channel_a_data.size){
+                    is(0.U){ size := 0.U }
+                    is(1.U){ size := 1.U }
+                    is(2.U){ size := 3.U }
+                }
                 switch(io.channel_a_data.opcode){
                     is(channel_a_opcodes.GET){
-                        stateReg := data_get
-                        addrLSBShift := "b11100100".U
+                        stateReg := data_get_start
+                        bitCnt := 0.U
                     }
                 }
             }
         }
-        is(data_get){
-            when(addrLSBShift(1, 0) === 3.U){
-                addrLSBShift := Cat(addrLSBShift(1, 0), addrLSBShift(7, 2))
+        is(data_get_start){
+            memAddress  := newMemAddress
+            stateReg    := data_get_cont
+        }
+        is(data_get_cont){
+            dataBuffer(memAddress(1, 0))    := io.memory_channel.read_data
+            memAddress                      := newMemAddress
+            when(bitCnt === size){
+                
             }
         }
     }
