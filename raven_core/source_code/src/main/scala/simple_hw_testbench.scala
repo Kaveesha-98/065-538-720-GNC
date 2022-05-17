a8e699b436192ecd4f49fe96bcc5c7b1e038db22
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2022-05-17 22:56:26 +0530

    simple hardware test bench

---
+++ raven_core/source_code/src/main/scala/simple_hw_testbench.scala
@@ -0,0 +1,47 @@
+//simple test bench to test raven_core
+//no ddr memory
+//no data memory(loads and stores to memory not possible)
+//instructions loaded via UART
+
+import chisel3._
+import chisel3.util._
+import chisel3.Driver
+
+class simple_hw_testBench(uartFrequency: Int, uartBaudRate: Int, instructionCount: Int) extends Module{
+    val io = IO(new Bundle{
+        val rxd = Input(UInt(1.W))
+        val txd = Output(UInt(1.W))
+    })
+
+    val instructionMemory = Reg(Vec(instructionCount, UInt(32.W)))
+    
+    //Release a byte at a time
+    val uartRecieveInstructions = Module(new Rx(uartFrequency, uartBaudRate))
+    
+    //Bytes will be buffered to form one instruction
+    val newInstruction = RegInit(0.U(32.W))
+    //For waiting 4 bytes before storing recieved instruction
+    val instructionByteCount = RegInit(0.U(3.W))
+
+    //cannot recive instruction byte until fully formed instruction is stored
+    val instructionIncomplete = instructionByteCount =/= 4.U
+    uartRecieveInstructions.io.channel.ready := instructionIncomplete
+
+    val newInstructionAddress = RegInit(0.U(32.W))
+
+    when(uartRecieveInstructions.io.channel.valid && instructionIncomplete){
+        instructionByteCount    := instructionByteCount + 1.U 
+        newInstruction          := Cat(uartRecieveInstructions.io.channel.bits, newInstruction(31, 8))
+    }.elsewhen(~instructionIncomplete){
+        instructionMemory(newInstructionAddress) := newInstruction
+        //for next instruction
+        instructionByteCount    := 0.U
+        newInstructionAddress   := newInstructionAddress + 1.U
+    }
+
+    val tx = Module(new BufferedTx(frequency, baudRate))
+    io.txd := tx.io.txd
+
+    val ravenHartCore = Module(new raven_core_hart())
+
+}
