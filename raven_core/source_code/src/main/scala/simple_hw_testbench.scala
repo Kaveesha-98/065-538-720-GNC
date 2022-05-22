92643ef9cab265e542f001e5ffc48298ea667e52
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2022-05-22 19:07:15 +0530

    removed lui instruction, not working with hart

--- raven_core/source_code/src/main/scala/simple_hw_testbench.scala
+++ raven_core/source_code/src/main/scala/simple_hw_testbench.scala
@@ -12,6 +12,9 @@ class simple_hw_testBench(uartFrequency: Int, uartBaudRate: Int, instructionCoun
         val rxd = Input(UInt(1.W))
         val txd = Output(UInt(1.W))
         val startProgram = Input(Bool())
+
+        val Led = Output(UInt(8.W))
+        val sw = Input(UInt(7.W))
     })
 
     val instructionMemory = Reg(Vec(instructionCount, UInt(32.W)))
@@ -24,21 +27,21 @@ class simple_hw_testBench(uartFrequency: Int, uartBaudRate: Int, instructionCoun
     //Bytes will be buffered to form one instruction
     val newInstruction = RegInit(0.U(32.W))
     //For waiting 4 bytes before storing recieved instruction
-    val instructionByteCount = RegInit(0.U(3.W))
+    val instructionBitCount = RegInit(0.U(6.W))
 
     //cannot recive instruction byte until fully formed instruction is stored
-    val instructionIncomplete = instructionByteCount =/= 4.U
+    val instructionIncomplete = instructionBitCount =/= 32.U
     uartRecieveInstructions.io.channel.ready := instructionIncomplete
 
     val newInstructionAddress = RegInit(0.U(32.W))
 
     when(uartRecieveInstructions.io.channel.valid && instructionIncomplete){
-        instructionByteCount    := instructionByteCount + 1.U 
-        newInstruction          := Cat(uartRecieveInstructions.io.channel.bits, newInstruction(31, 8))
+        instructionBitCount    := instructionBitCount + 1.U 
+        newInstruction          := Cat((uartRecieveInstructions.io.channel.bits === '1'.U).asUInt, newInstruction(31, 1))
     }.elsewhen(~instructionIncomplete){
         instructionMemory(newInstructionAddress) := newInstruction
         //for next instruction
-        instructionByteCount    := 0.U
+        instructionBitCount    := 0.U
         newInstructionAddress   := newInstructionAddress + 1.U
     }
 
@@ -60,19 +63,34 @@ class simple_hw_testBench(uartFrequency: Int, uartBaudRate: Int, instructionCoun
     ravenHartCore.io.load_data_valid := 1.U
     ravenHartCore.io.load_data := 0.U 
 
-    ravenHartCore.io.cache_address_channel.ARREADY := true.B
+    ravenHartCore.io.fetch_PC := ravenHartCore.io.valid_PC
+    ravenHartCore.io.fetch_valid := ravenHartCore.io.ready & io.startProgram.asUInt
+    ravenHartCore.io.instruction := instructionMemory(ravenHartCore.io.valid_PC(31, 2))
+
     
-    ravenHartCore.io.cache_data_channel.RDATA := instructionMemory(ravenHartCore.io.cache_address_channel.ARADDR)
+    val memOut = instructionMemory(io.sw(6, 2))
+    val ledOut = WireInit(memOut(7, 0))
 
-    ravenHartCore.io.cache_data_channel.RVALID := io.startProgram
+    switch(io.sw(1, 0)){
+        is("b01".U){
+            ledOut := memOut(15, 8)
+        }
+        is("b10".U){
+            ledOut := memOut(23, 16)
+        }
+        is("b11".U){
+            ledOut := memOut(31, 24)
+        }
+    }
 
+    io.Led := ledOut
 }
 
 object simple_hw_testBench extends App{
 
     val uartFrequency: Int = 100000000
     val uartBaudRate: Int = 115200
-    val instructionCount: Int = 4
+    val instructionCount: Int = 15
 
     (new chisel3.stage.ChiselStage).emitVerilog(new simple_hw_testBench(uartFrequency, uartBaudRate, instructionCount))
 }
