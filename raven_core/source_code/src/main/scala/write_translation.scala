0c7da6fc89846a9988758382734068d6ab9801f7
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2022-05-28 18:59:18 +0530

    started decoding hart instruction to mem write access

--- raven_core/source_code/src/main/scala/write_translation.scala
+++ raven_core/source_code/src/main/scala/write_translation.scala
@@ -6,6 +6,8 @@ import chisel3._
 import chisel3.util._
 import chisel3.Driver
 
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - Bundle - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
 class write_translation_address_channel(addressWidth: Int, sizeWidth: Int) extends Bundle{
     //input to write translation module
 
@@ -18,6 +20,8 @@ class write_translation_address_channel(addressWidth: Int, sizeWidth: Int) exten
 
 }
 
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - Bundle - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
 class write_translation_data_channel(dataWidth: Int) extends Bundle{
     //input to write translation module
 
@@ -29,6 +33,8 @@ class write_translation_data_channel(dataWidth: Int) extends Bundle{
 
 }
 
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - Bundle - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
 class write_translation_write_instruction(addressWidth:Int, sizeWidth: Int, dataWidth:Int) extends Bundle{
     //Stores data needed to carry memory write access
     //Used to create buffers for memory access issue
@@ -39,20 +45,69 @@ class write_translation_write_instruction(addressWidth:Int, sizeWidth: Int, data
     val hart_data_out = UInt(dataWidth.W)
 }
 
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - Bundle - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
 class write_translation_intra_handshake() extends Bundle{
     val valid = Input(Bool())
     val ready = Output(Bool())
 }
 
-class write_translation_mem_write_interface(addressWidth: Int){
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - Bundle - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+class write_translation_mem_write_interface_data(dataWidth: Int) extends Bundle{
     //address width must be divisible by 8 for correct mask length
     assert (addressWidth % 8 == 0)
 
-    val address = UInt(addressWidth.W)
+    val data = UInt(addressWidth.W)
     val mask = UInt((addressWidth/8).W)
     val en = Bool()
 
 }
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - Bundle - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+class xilinx6_mig_write_data_access_signals(dataWidth: Int) extends Bundle{
+    //address width must be divisible by 8 for correct mask length
+    assert (addressWidth % 8 == 0)
+
+    val data = UInt(addressWidth.W)
+    val mask = UInt((addressWidth/8).W)
+
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - Bundle - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+class xilinx6_mig_write_data_port_hart_signals(dataWidth:Int) extends Bundle{
+
+    val access_signals = Input(new xilinx_mig_write_data_access_signals(dataWidth))
+
+    val en = Input(Bool())
+    val full = Output(Bool())
+
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - Bundle - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+class xilinx6_mig_command_access_signals() extends Bundle{
+
+    val addr = UInt(30.W)
+    val instr = UInt(3.W)
+    val bl = UInt(6.W)
+
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - Bundle - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+class xilinx6_mig_command_data_port_hart_signals() extends Bundle{
+
+    val access_signals = Input(new xilinx6_mig_command_access_signals())
+
+    val en = Input(Bool())
+    val full = Output(Bool())
+
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * Module * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 class write_translation_write_buffer(addressWidth:Int, sizeWidth: Int, dataWidth) extends Module{
     //stores the write issue from hart, then forwards it to memory or io
@@ -108,6 +163,8 @@ class write_translation_write_buffer(addressWidth:Int, sizeWidth: Int, dataWidth
 
 }
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * Module * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 Class write_translation_mem_access(addressWidth: Int, sizeWidth: Int, dataWidth: Int) extends Module{
     //performs mem write access
     val io = IO(new Bundle{
@@ -115,7 +172,38 @@ Class write_translation_mem_access(addressWidth: Int, sizeWidth: Int, dataWidth:
         
         val mainControlHandShake = new write_translation_intra_handshake()
 
-        val memWriteInterface = Output(new write_translation_mem_access(addressWidth))
-        val memWriteBufferFull = Input(Bool()) 
+        val memWriteInterfaceData = Flipped(new xilinx6_mig_write_data_port_hart_signals(dataWidth))
+
+        val memWriteInterfaceCmd = Flipped(new xilinx6_mig_command_data_port_hart_signals())
     })
+
+    def checkMemAlignment[T <: Data]
+
+    val dataPortAccessSignalsBuffer = Reg(Vec(2, xilinx6_mig_write_data_access_signals(dataWidth)))
+    val commandPortAccessSignalBuffer = Reg(new xilinx6_mig_command_access_signals())
+
+    //states of the module
+    val idle :: issuingData :: issuingCommand :: Nil = Enum(3)
+    val stateReg = RegInit(idle)
+
+    io.mainControlHandShake.ready := stateReg === idle
+
+    val accessMisaligned = RegInit(Bool())
+
+    switch(stateReg){
+        is(idle){
+            when(io.mainControlHandShake.valid){
+                //only supported for 32bit ports
+                commandPortAccessSignalBuffer.addr := Cat(io.writeInstructionForward.address(29, 2), "b00")
+                commandPortAccessSignalBuffer.instr := "b000".U
+                //setting burst length - misaligned access "b001".U else "b000000".U
+                commandPortAccessSignalBuffer.bl := "b000000".U
+                switch(io.writeInstructionIn.size(1, 0)){
+                    is("b01".U){
+                        commandPortAccessSignalBuffer.bl := Mux()
+                    }
+                }
+            }
+        }
+    }
 }
